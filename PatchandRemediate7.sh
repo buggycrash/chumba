44cf2f8f771fbd0711fcf2a34a5d30c8e1efd840
buggycrash <buggycrash@yahoo.com>
2017-07-17 20:49:43 -0400

    Stable on Official CentOS 7 minimal AMI
    More checks, and a fix for DefaultZone=drop

--- PatchandRemediate7.sh
+++ PatchandRemediate7.sh
@@ -11,7 +11,7 @@
 # Running after host modification (such as adding httpd or other installs) may create changes leading to unexpected behaviour.
 #
 # --------------------------------------------------------
-# Human Safety Valve
+# *****Human Safety Valve
 # Make sure user is aware of changes
 function ask_yes_or_no() {
     read -p "$1 ([y]es or [N]o): "
@@ -29,7 +29,7 @@ then
     exit 0
 fi
 #
-# Human Safety valve.  Comment the entire if statement above to enable the script to run automatically via User Data.
+# *****Human Safety Valve.  Comment the entire if statement above to enable the script to run automatically via User Data.
 #
 if ! grep 'CentOS Linux release 7' /etc/redhat-release; then
     echo "Script exited.  This remediation script is only designed to work on CentOS 7.  No changes made."
@@ -37,14 +37,16 @@ if ! grep 'CentOS Linux release 7' /etc/redhat-release; then
 fi
 echo ""
 echo "Beginning patching and remediation.  The process may take several minutes if a large number of patches need to be applied."
-sleep 1
+sleep 2
 #
 # Install epel repository, needed for some of the scap-scanner and SSG dependencies
 #
 echo "Installing requirements...."
+sleep 1
 yum -y install epel-release
 #
 # Add newline to sshd_config to ensure that remediate won't break SSH and brick the host.
+# In previous efforts, the remediation script expected a newline at the end of the sshd_config file but it did not always exist.
 echo "" >> /etc/ssh/sshd_config
 #
 # Install openscap-scanner, scap-security-guide, and ntp packages.  Ntp is not installed by default on some minimal AMIs.
@@ -52,29 +54,33 @@ yum -y install openscap-scanner scap-security-guide ntp
 #
 # Update all packages
 clear
-sleep 1
 echo "Updating patches.  This may take some time...."
+sleep 2
 yum -y update
 #
 # Remediate using STIG SCAP Profile provided by Red Hat, based on the DISA STIG SCAP Profile.  
 # This implementation will not fully meet DoD or IC requirements, but will provide a very close representative baseline.  
 #
 clear
-sleep 1
 echo "Evaluating the system and then Remediating discovered vulnerabilities...." 
+sleep 2
 if ! grep 'CentOS Linux release 7' /etc/redhat-release; then
     echo "Script exited.  This remediation script is only designed to work on CentOS 7."
     exit 0
-   elif grep 'CentOS Linux release 7' /etc/redhat-release; then
+   elif grep 'CentOS Linux release 7' /etc/redhat-release && ! grep 'ZONE' /etc/sysconfig/network-scripts/ifcfg-eth0; then
 	yum -y install firewalld
-	# firewalld is not installed by default on some minimal AMIs, yet the remediation assumes it is available
+	echo "" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+	echo 'ZONE="public"' >> /etc/sysconfig/network-scripts/ifcfg-eth0
+	# firewalld is not installed by default on some minimal AMIs, yet the remediation assumes it is available.
+	# The OpenSCAP remediation sets DefaultZone=drop in /etc/firewalld.conf so we need to ensure eth0 already has a defined zone.  By default in a new install, none is defined.
+	# In a default install, the public zone is already defined and allows only ssh and client.IPv6 which is why we set ZONE=public rather than some other value.
 	oscap xccdf eval --remediate --profile xccdf_org.ssgproject.content_profile_stig-rhel7-server-upstream /usr/share/xml/scap/ssg/content/ssg-centos7-ds.xml
 fi
 #
 # Evaluate and generate html report
 clear
-sleep 1
 echo "Generating remediation report...."
+sleep 2
 if ! grep 'CentOS Linux release 7' /etc/redhat-release; then
     echo "Script exited.  This version is only designed to work on CentOS 7."
     exit 0
@@ -84,6 +90,9 @@ fi
 #
 # Recommend reboot for kernal update and audit update
 echo ""
+echo "--------------------------------------------------------------------------------------------------"
 echo "The scripted portion of remediation is complete."
 echo "Please reboot in order to apply the kernal updates, enable compliant auditing, and implement other fixes which can only be applied at boot time."
 echo "A STIG compliance report (status before the reboot) is available at /tmp/remediation-results.html"
+echo "--------------------------------------------------------------------------------------------------"
+echo ""
